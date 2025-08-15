f94dcfb61e25ca63fa67851898435c8a6802672e
Raginaakhter <raginaakhter052@gmail.com>
2025-08-15 21:00:45 +0600

    finised

--- src/firebase/Firebase.config.js
+++ src/firebase/Firebase.config.js
@@ -6,13 +6,13 @@ import { getAuth } from "firebase/auth";
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCAB7T0Qp6JsDvnWbS8ySEat1xmML5sKXc",
-  authDomain: "user-pass-email-a55f5.firebaseapp.com",
-  projectId: "user-pass-email-a55f5",
-  storageBucket: "user-pass-email-a55f5.firebasestorage.app",
-  messagingSenderId: "539609320127",
-  appId: "1:539609320127:web:2e3891d10a6de1b487540a",
-  measurementId: "G-S9MKC2D56V"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 
